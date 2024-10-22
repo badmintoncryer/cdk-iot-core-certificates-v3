f1f7f11fa051c1bc1aa019643230f80995faee4a
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-10-22 21:16:05 +0900

    initial commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,16 +2,29 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Kazuho CryerShinozuka',
   authorAddress: 'malaysia.cryer@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.80.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.5.0',
   name: 'cdk-iot-core-certificates-v3',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/badmintoncryer/cdk-iot-core-certificates-v3.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  keywords: ['aws', 'cdk', 'ec2', 'aws-cdk'],
+  gitignore: ['*.js', '*.d.ts', '!test/.*.snapshot/**/*', '.tmp'],
+  deps: [],
+  description: 'CDK Construct for AWS IoT Core certificates and things',
+  devDeps: [
+    '@aws-cdk/integ-runner@2.80.0-alpha.0',
+    '@aws-cdk/integ-tests-alpha@2.80.0-alpha.0',
+  ],
+  releaseToNpm: true,
+  packageName: 'cdk-iot-core-certificates-v3',
+  publishToPypi: {
+    distName: 'cdk-iot-core-certificates-v3',
+    module: 'cdk-iot-core-certificates-v3',
+  },
 });
+project.projectBuild.testTask.exec(
+  'yarn tsc -p tsconfig.dev.json && yarn integ-runner',
+);
 project.synth();
